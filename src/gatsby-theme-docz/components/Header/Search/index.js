d21eda23cbb89d53fe99247f5c25e12dc5030164
chrissie fernando <chrissie@cartoonmango.com>
2020-08-31 20:23:57 +0530

    [ADDED] - agolia search

--- src/gatsby-theme-docz/components/Header/Search/index.js
+++ src/gatsby-theme-docz/components/Header/Search/index.js
@@ -1,55 +1,57 @@
-import React, { useState, useEffect, createRef } from 'react'
+import React, { useState, useEffect, createRef } from "react";
 import {
   InstantSearch,
   Index,
   Hits,
   connectStateResults,
-} from 'react-instantsearch-dom'
-import algoliasearch from 'algoliasearch/lite'
-import * as styles from 'gatsby-theme-docz/src/components/NavSearch/styles'
-import { Root, HitsWrapper, PoweredBy } from './primitives'
-import Input from './Input'
-import * as hitComps from './hitComps'
+} from "react-instantsearch-dom";
+import { useColorMode } from "theme-ui";
+import algoliasearch from "algoliasearch/lite";
+import * as styles from "gatsby-theme-docz/src/components/NavSearch/styles";
+import { Root, HitsWrapper, PoweredBy } from "./primitives";
+import Input from "./Input";
+import * as hitComps from "./hitComps";
 
 const Results = connectStateResults(
   ({ searchState: state, searchResults: res, children }) =>
     res && res.nbHits > 0 ? children : `No results for '${state.query}'`
-)
+);
 
 const Stats = connectStateResults(
   ({ searchResults: res }) =>
     res && res.nbHits > 0 && `${res.nbHits} result${res.nbHits > 1 ? `s` : ``}`
-)
+);
 
 const useClickOutside = (ref, handler, events) => {
-  if (!events) events = [`mousedown`, `touchstart`]
-  const detectClickOutside = event =>
-    !ref.current.contains(event.target) && handler()
+  if (!events) events = [`mousedown`, `touchstart`];
+  const detectClickOutside = (event) =>
+    !ref.current.contains(event.target) && handler();
   useEffect(() => {
     for (const event of events)
-      document.addEventListener(event, detectClickOutside)
+      document.addEventListener(event, detectClickOutside);
     return () => {
       for (const event of events)
-        document.removeEventListener(event, detectClickOutside)
-    }
-  })
-}
+        document.removeEventListener(event, detectClickOutside);
+    };
+  });
+};
 const indices = [
   {
     name: process.env.GATSBY_ALGOLIA_INDEX_NAME,
-    title: `docz algolia example`,
+    title: `Search Results`,
     hitComp: `PageHit`,
   },
-]
+];
 export const NavSearch = ({ collapse, hitsAsGrid }) => {
-  const ref = createRef()
-  const [query, setQuery] = useState(``)
-  const [focus, setFocus] = useState(false)
+  const [colorMode] = useColorMode();
+  const ref = createRef();
+  const [query, setQuery] = useState(``);
+  const [focus, setFocus] = useState(false);
   const searchClient = algoliasearch(
     process.env.GATSBY_ALGOLIA_APP_ID,
     process.env.GATSBY_ALGOLIA_SEARCH_KEY
-  )
-  useClickOutside(ref, () => setFocus(false))
+  );
+  useClickOutside(ref, () => setFocus(false));
   return (
     <InstantSearch
       searchClient={searchClient}
@@ -57,8 +59,16 @@ export const NavSearch = ({ collapse, hitsAsGrid }) => {
       onSearchStateChange={({ query }) => setQuery(query)}
     >
       <div style={styles.wrapper} ref={ref}>
-        <Input onFocus={() => setFocus(true)} {...{ collapse, focus }} />
-        <HitsWrapper show={query.length > 0 && focus} asGrid={hitsAsGrid}>
+        <Input
+          onFocus={() => setFocus(true)}
+          {...{ collapse, focus }}
+          colorMode={colorMode}
+        />
+        <HitsWrapper
+          show={query.length > 0 && focus}
+          asGrid={hitsAsGrid}
+          colorMode={colorMode}
+        >
           {indices.map(({ name, title, hitComp }) => (
             <Index key={name} indexName={name}>
               <header>
@@ -74,6 +84,6 @@ export const NavSearch = ({ collapse, hitsAsGrid }) => {
         </HitsWrapper>
       </div>
     </InstantSearch>
-  )
-}
-export default NavSearch
+  );
+};
+export default NavSearch;
