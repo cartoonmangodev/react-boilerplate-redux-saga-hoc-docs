489357928f490e9ec1f717794596156e24bfdb97
chrissie fernando <chrissie@cartoonmango.com>
2020-08-31 19:48:26 +0530

    [ADDED] - agolia search

--- src/gatsby-theme-docz/components/Header/index.js
+++ src/gatsby-theme-docz/components/Header/index.js
@@ -1,24 +1,29 @@
 /** @jsx jsx */
-import { jsx, Box, Flex, useColorMode } from 'theme-ui'
-import { useConfig, useCurrentDoc } from 'docz'
+import { jsx, Box, Flex, useColorMode } from "theme-ui";
+import { useConfig, useCurrentDoc } from "docz";
 
-import * as styles from 'gatsby-theme-docz/src/components/Header/styles'
-import { Edit, Menu, Sun, Github } from 'gatsby-theme-docz/src/components/Icons'
-import { Logo } from 'gatsby-theme-docz/src/components/Logo'
-import Search from './Search/'
+import * as styles from "gatsby-theme-docz/src/components/Header/styles";
+import {
+  Edit,
+  Menu,
+  Sun,
+  Github,
+} from "gatsby-theme-docz/src/components/Icons";
+import { Logo } from "gatsby-theme-docz/src/components/Logo";
+import Search from "./Search/";
 
-export const Header = props => {
-  const { onOpen } = props
+export const Header = (props) => {
+  const { onOpen } = props;
   const {
     repository,
     themeConfig: { showDarkModeSwitch, showMarkdownEditButton },
-  } = useConfig()
-  const { edit = true, ...doc } = useCurrentDoc()
-  const [colorMode, setColorMode] = useColorMode()
+  } = useConfig();
+  const { edit = true, ...doc } = useCurrentDoc();
+  const [colorMode, setColorMode] = useColorMode();
 
   const toggleColorMode = () => {
-    setColorMode(colorMode === 'light' ? 'dark' : 'light')
-  }
+    setColorMode(colorMode === "light" ? "dark" : "light");
+  };
 
   return (
     <div sx={styles.wrapper} data-testid="header">
@@ -31,10 +36,10 @@ export const Header = props => {
         <Logo />
 
         <Flex>
-          {repository && (
+          {
             <Box sx={{ mr: 2 }}>
               <a
-                href={repository}
+                href="https://github.com/cartoonmangodev/react-boilerplate-redux-saga-hoc"
                 sx={styles.headerButton}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -42,13 +47,13 @@ export const Header = props => {
                 <Github size={15} />
               </a>
             </Box>
-          )}
+          }
           <Search />
-          {showDarkModeSwitch && (
-            <button sx={styles.headerButton} onClick={toggleColorMode}>
-              <Sun size={15} />
-            </button>
-          )}
+          {/* {showDarkModeSwitch && ( */}
+          <button sx={styles.headerButton} onClick={toggleColorMode}>
+            <Sun size={15} />
+          </button>
+          {/* )} */}
         </Flex>
         {showMarkdownEditButton && edit && doc.link && (
           <a
@@ -63,5 +68,5 @@ export const Header = props => {
         )}
       </div>
     </div>
-  )
-}
+  );
+};
